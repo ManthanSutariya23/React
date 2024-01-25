74d91d3b80baffc9f4eb1d7d9ff2121bea829efe
ManthanSutariya23 <manthan.j.sutariya@gmail.com>
2024-01-25 19:07:39 +0000

    create route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./Home";
+import Name from "./Name";
+
 function App() {
-  return <div>Manthan</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" component={<Home />} />
+          <Route path="/home" component={<Home />} />
+          <Route path="/name" component={<Name />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <h1>Welcome to home page</h1>
+      <Link to={"/name"} />
+    </>
+  );
+}
+
+export default Home;
