4265ccfccb6928647f57f7f0673de903ec824f58
ManthanSutariya23 <manthan.j.sutariya@gmail.com>
2024-01-25 21:06:21 +0000

    chnage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/name" element={<Name />} />
         </Routes>
       </Router>
